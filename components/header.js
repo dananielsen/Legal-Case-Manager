aa9041c6eb7cb20c9427d6653aa07093d1b08193
Dstillin <dstillin@asu.edu>
2023-03-07 22:53:11 -0500

    fixed issue - login works with user displayed in header

--- components/header.js
+++ components/header.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,7 +6,8 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { parseCookies } from 'nookies';
+import { parseCookies } from "nookies";
+import Link from "next/link"
 
 export default function ButtonAppBar() {
     const cookies = parseCookies()
@@ -26,12 +27,16 @@ export default function ButtonAppBar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+           
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            {user && user.email}
           </Typography>
+          <Link href = "/login">
           <Button color="inherit">Login</Button>
+          </Link> <Link href = "/register">
+          <Button color="inherit">Register</Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
