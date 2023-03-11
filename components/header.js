be38de139ff02ec4fd48a485400ef58a3499759c
Dstillin <dstillin@asu.edu>
2023-03-10 21:46:16 -0500

    login, logout

--- components/header.js
+++ components/header.js
@@ -8,14 +8,35 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { parseCookies } from "nookies";
 import Link from "next/link"
+import { useSession } from "next-auth/react"
+import cookie from "js-cookie"
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
 
 export default function ButtonAppBar() {
     const cookies = parseCookies()
+    const router = useRouter()
+    const [userState, setUserState] = useState("")
 
-    const user = cookies?.user ? JSON.parse(cookies.user) : ""
     
-    console.log(user)
+    
+    const { data: session } = useSession()
+    console.log(session)
+    const user = cookies?.user ? JSON.parse(cookies.user) : ""
+    console.log(userState)
+    useEffect(() => {
+      setUserState(user)
+    }, [router])
 
+    const logoutHandler = async () => {
+      if(session) {
+        signOut()
+      }
+      cookie.remove('token')
+      cookie.remove('user')
+      setUserState("")
+    }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -30,13 +51,21 @@ export default function ButtonAppBar() {
            
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {user && user.email}
+            {userState && userState.email}
           </Typography>
-          <Link href = "/login">
+          {user ? (
+          <> 
+          <Button color="inherit" onClick={logoutHandler}>Logout</Button>
+         
+          </>
+          ) :  (
+          <><Link href = "/login">
           <Button color="inherit">Login</Button>
           </Link> <Link href = "/register">
           <Button color="inherit">Register</Button>
           </Link>
+          </>)}
+         
         </Toolbar>
       </AppBar>
     </Box>
--- pages/login.js
+++ pages/login.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 import axios from "axios"
 import cookie from 'js-cookie'
 import { parseCookies } from 'nookies';
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/router'
 
 
 const login = () => {
     const [email, setEmail ] = useState("")
     const [password, setPassword ] = useState("")
+    const { data: session } = useSession()
+    const router = useRouter()
 
   const cookies = parseCookies()
 
@@ -24,9 +28,26 @@ const login = () => {
       
         cookie.set("token", data?.token)
         cookie.set("user", JSON.stringify(data?.user))
+        router.push('/')
         console.log(email, password)
       }
 
+      const logoutHandler = async () => {
+        if(session) {
+          signOut()
+        }
+        cookie.remove('token')
+        cookie.remove('user')
+      }
+      if (session)  {
+        return (
+          <>
+          Signed in as { session.user.email} <br />
+          <button onClick={logoutHandler}>Sign out</button>
+          
+          </>
+        )
+      }
        
    
   return (
